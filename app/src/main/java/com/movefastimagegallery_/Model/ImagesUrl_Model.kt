de0bb6abf57acacd13588492f14737c84d48d5bf
Muhammad Naeem <naeem0313@gmail.com>
2019-10-12 13:49:22 +0500

    Create Pojo class for Photo API response

---
+++ app/src/main/java/com/movefastimagegallery_/Model/ImagesUrl_Model.kt
@@ -0,0 +1,20 @@
+package com.movefastimagegallery_.Model
+
+
+import java.io.Serializable
+
+class ImagesUrl_Model : Serializable {
+
+
+    var user_id: String? = null
+    var photo_id: String? = null
+    var username: String? = null
+    var location: String? = null
+    var profileImage: String? = null
+    var description: String? = null
+
+    var full: String? = null
+    var regular: String? = null
+    var small: String? = null
+    var thumb: String? = null
+}
